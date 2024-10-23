eb362e7c861b42b1a8f9ed306cd9e4dfd97bc73a
qinhj <qhj@systech.com.cn>
2024-10-23 13:29:29 +0800

    新增路由及格式化

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,39 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
-import { basicAuth } from "hono/basic-auth";
+import { serve, type HttpBindings } from '@hono/node-server';
+import { serveStatic } from '@hono/node-server/serve-static';
+import { Hono } from 'hono';
+import { basicAuth } from 'hono/basic-auth';
 
-const app = new Hono();
+type Bindings = HttpBindings & {
+  /* ... */
+};
 
-app.get("/", (c) => {
-  return c.text("Hello Hono 666!");
-});
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get('/', (c) => {
-  return new Response('Good morning!')
-})
+  return c.text('Hello Hono 666!');
+});
+
+// 访问原始 Node.js API
+app.get('/', (c) => {
+  return c.json({
+    remoteAddress: c.env.incoming.socket.remoteAddress,
+  });
+});
 
 // 返回 JSON
-app.get("/api/hello", (c) => {
+app.get('/api/hello', (c) => {
   return c.json({
     ok: true,
-    message: "Hello Hono!",
+    message: 'Hello Hono!',
   });
 });
 
 // post请求
-app.post("/posts", (c) => c.text("Created!", 201));
+app.post('/posts', (c) => c.text('Created!', 201));
 // 携带参数
-app.delete("/posts/:id", (c) => c.text(`${c.req.param("id")} is deleted!`));
+app.delete('/posts/:id', (c) => c.text(`${c.req.param('id')} is deleted!`));
 
-
-// 返回 HTML
+// 使用 TSX 返回 HTML
 const View = () => {
   return (
     <html>
@@ -34,11 +41,11 @@ const View = () => {
         <h1>Hello Hono!</h1>
       </body>
     </html>
-  )
-}
+  );
+};
 app.get('/page', (c) => {
-  return c.html(<View />)
-})
+  return c.html(<View />);
+});
 
 // 使用 中间件
 app.use(
@@ -47,11 +54,30 @@ app.use(
     username: 'admin',
     password: 'secret',
   })
-)
+);
 // 访问页面即可查看效果
 app.get('/admin', (c) => {
-  return c.text('You are authorized!')
-})
+  return c.text('You are authorized!');
+});
+
+app.get(
+  '/static/*',
+  serveStatic({
+    root: './',
+    rewriteRequestPath: (path) => path.replace(/^\/static/, '/statics'),
+  })
+);
+
+// 指定基本路径
+const api = new Hono().basePath('/api');
+api.get('/book', (c) => c.text('List Books'));
+
+// 使用路由分组
+const user = new Hono().basePath('/user');
+user.get('/', (c) => c.text('List Users')); // GET /user
+user.post('/', (c) => c.text('Create User')); // POST /user
+// 使用路由
+app.route('/', user);
 
 const port = 3000;
 
